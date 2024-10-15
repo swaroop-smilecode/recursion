15ec21c5c969a9ed9817e270a9de1163afb4b282
swaroop <swaroopiit@gmail.com>
2024-10-15 16:34:17 +0530

    Solved some more problems

--- 2. Problems/p1_sum_numbers_recursive.py
+++ 2. Problems/p1_sum_numbers_recursive.py
@@ -1,7 +1,7 @@
 def sum_numbers_recursive(numbers):
-  if len(numbers) == 0:
+  if numbers == []:
     return 0
   
   return numbers[0] + sum_numbers_recursive(numbers[1:])
 
-sum_numbers_recursive([5, 2, 9, 10]) # -> 26
+print(sum_numbers_recursive([5, 2, 9, 10])) # -> 26
